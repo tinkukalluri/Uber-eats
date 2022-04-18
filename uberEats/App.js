957a2ab6950eca87287dfea5b92151233eb77cca
tinkukalluri <tinkukalluri19859@gmail.com>
2022-04-19 00:32:57 +0530

    Screen/Home

---
+++ uberEats/App.js
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+import Home from "./screens/Home";
+
+export default function App() {
+  return <Home />
+
+}
---
+++ uberEats/screens/Home.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { Divider } from "react-native-elements";
+import Categories from "../components/Home/Categories";
+// import BottomTabs from "../components/home/BottomTabs";
+import HeaderTab from "../components/Home/HeaderTab";
+import RestaurantItem, {
+    localRestaurants,
+} from "../components/Home/RestaurantItem";
+import SearchBar from "../components/Home/SearchBar";
+
+const YELP_API_KEY =
+    "bdRJutLhFAQJ36t7b89CWjHFBU4OKzjt9wvZzcY-nkgmvTqlNMjZWV1eG7iBQ9R74SyfxRg9LWnBAkZY06BtAZAe4d2dfX-2vuX8a1l5V7foctHfX9UKEyoM5ts3YXYx";
+
+export default function Home({ navigation }) {
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
+    const [city, setCity] = useState("San Francisco");
+    const [activeTab, setActiveTab] = useState("Delivery");
+
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+        const apiOptions = {
+            headers: {
+                Authorization: `Bearer ${YELP_API_KEY}`,
+            },
+        };
+
+        return fetch(yelpUrl, apiOptions)
+            .then((res) => res.json())
+            .then((json) =>
+                setRestaurantData(
+                    json.businesses.filter((business) =>
+                        business.transactions.includes(activeTab.toLowerCase())
+                    )
+                )
+            );
+    };
+
+    useEffect(() => {
+        getRestaurantsFromYelp();
+    }, [city, activeTab]);
+
+    return (
+        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15 }}>
+                <HeaderTab activeTab={activeTab} setActiveTab={setActiveTab} />
+                <SearchBar cityHandler={setCity} />
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItem
+                    restaurantData={restaurantData}
+                    navigation={navigation}
+                />
+            </ScrollView>
+            <Divider width={1} />
+            {/* <BottomTabs /> */}
+        </SafeAreaView>
+    );
+}
