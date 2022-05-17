7dbd4336576454c65c4ead50f5beba680c389497
tinkukalluri <tinkukalluri19859@gmail.com>
2022-05-17 13:23:04 +0530

    .

--- rough.js
+++ rough.js
@@ -2,25 +2,38 @@
 
 
 
-snapshot = {
-    docs: [{
-        id: 1,
-        userdata: {
-            name: "tinku",
-            age: "21"
-        },
-        data: () => {
-            return snapshot.docs[0].userdata
-        }
-    }
-    ]
+// snapshot = {
+//     docs: [{
+//         id: 1,
+//         userdata: {
+//             name: "tinku",
+//             age: "21"
+//         },
+//         data: () => {
+//             return snapshot.docs[0].userdata
+//         }
+//     }
+//     ]
+// }
+
+// json_str_data = JSON.stringify(snapshot)
+// json_parse_obj = JSON.parse(json_str_data)
+
+// new_list = snapshot.docs.map((doc) => { return doc.data() })
+// console.log(new_list)
+
+props = {
+    tinku: "kalluri",
+    sindhu: "sintin"
 }
 
-json_str_data = JSON.stringify(snapshot)
-json_parse_obj = JSON.parse(json_str_data)
 
-new_list = snapshot.docs.map((doc) => { return doc.data() })
-console.log(new_list)
+function fun1({ tinku, ...props }) {
+    console.log(tinku)
+    console.log(props.sindhu)
+}
+
+fun1(props)
 
 
 
